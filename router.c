799802282e6a54e8d25563e4a4f17c7835d62925
vladzinca <vlad_zinca@yahoo.com>
2022-04-12 17:56:06 +0300

    Attempts to implement very basic forwarding

--- router.c
+++ router.c
@@ -9,9 +9,22 @@ int main(int argc, char *argv[])
 	// Do not modify this line
 	init(argc - 2, argv + 2);
 
-	while (1) {
+	while (1)
+	{
 		rc = get_packet(&m);
 		DIE(rc < 0, "get_packet");
 		/* TODO */
+		struct ether_header *eth_hdr = (struct ether_header *)m.payload;
+		if (ntohs(eth_hdr->ether_dhost) == ntohs(eth_hdr->ether_shost) || ntohs(eth_hdr->ether_dhost) == INADDR_BROADCAST)
+		{
+			if (ntohs(eth_hdr->ether_type) == ETHERTYPE_IP)
+			{
+
+			}
+			else if (ntohs(eth_hdr->ether_type) == ETHERTYPE_ARP)
+			{
+
+			}
+		}
 	}
 }
